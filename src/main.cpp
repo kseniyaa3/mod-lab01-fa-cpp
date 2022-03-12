fc359336a249ec9fc25d7f0381da89deb4634938
kseniyaa3 <98958076+kseniyaa3@users.noreply.github.com>
2022-03-12 18:08:09 +0300

    changedmain3

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,9 @@
 #include "fun.h"
 
 int main() {
-    const char s[21] = { 's', 't', 'r', 'O', 'K', 'a', '3', 
-                        ' ', 'G', 'h', 't', 'Y', 
-                        ' ', 'n', 'k', 'i', '4', 'y', 
+    const char s[21] = { 's', 't', 'r', 'O', 'K', 'a', '3',
+                        ' ', 'G', 'h', 't', 'Y',
+                        ' ', 'n', 'k', 'i', '4', 'y',
                         ' ', '0', '\0' };
     faStr1(s);
     faStr2(s);
